b056ca7c1e792f9f3ed3b026af261581620e4235
CarlosTrancon <carlostala8@gmail.com>
2016-04-07 12:29:59 +0200

    commit

---
+++ calculadora/src/calculadora/principal.java
@@ -0,0 +1,12 @@
+package calculadora;
+
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculadora c = new Calculadora(6,3);
+		System.out.println(c.division());
+		System.out.println(c.suma());
+	}
+
+}
